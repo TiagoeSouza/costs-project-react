a5af107e07f2febf1ee687d23f237a29ee5955cb
tiagoesouza <tiago_e_souza@hotmail.com>
2023-09-08 23:31:24 -0300

    Adicionado componente de mensagens e ajustado formulario que listara os projetos cadastrados
    Signed-off-by: tiagoesouza <tiago_e_souza@hotmail.com>

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,36 @@
-function Projects(){
-    return (<div><h1>Projects</h1></div>)
+import Message from "../layout/Message";
+import { useLocation } from "react-router-dom";
+
+import styles from './Projects.module.css'
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+function Projects() {
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+        // Limpo o location, para que a mensagem não seja exibida novamante caso recarregue a pagina
+        location.state.message = "";
     }
-    
-    export default Projects;
+
+    return (
+        <>
+            {message && <Message type="success" text={message} />}
+            
+            <div className={styles.project_container}>
+                <div className={styles.title_container}>
+                    <h1>Meus Projetos</h1>
+                    <LinkButton to="/newproject" text="Criar Projeto" />
+
+                </div>
+
+                <Container customClass="start">
+                    <p>Projetos ...</p>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default Projects;
